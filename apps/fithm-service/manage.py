8a3bdfa1b0a74e07ce917a36067b77df31b2f38f
romulo114 <romulo.dev114@gmail.com>
2022-03-26 13:01:37 -0700

    Update service repo to create default values upon container initialization.
    Update docker-compose.yml to include script to add multiple databases.

--- apps/fithm-service/manage.py
+++ apps/fithm-service/manage.py
@@ -1,8 +1,22 @@
-from flask.cli import FlaskGroup
-
+from flask.cli import FlaskGroup, AppGroup
+from libs.database import create_tables, drop_tables, populate_default
 from main import app
 
 cli = FlaskGroup(app)
+
+db_cli = AppGroup('db')
+
+@db_cli.command('create')
+def create_db():
+    create_tables()
+    populate_default()
+
+@db_cli.command('drop')
+def drop_db():
+    drop_tables()
+
+app.cli.add_command(db_cli)
+
 
 if __name__ == '__main__':
     cli()
