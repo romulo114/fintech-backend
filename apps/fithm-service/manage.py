4c4a4a3bae7b0c82ad6356028fa7f13657a7e5b1
romulo114 <romulo.dev114@gmail.com>
2022-05-02 17:03:13 -0700

    Modify service configurations to use create app and separate database.
    Add fixtures.

--- apps/fithm-service/manage.py
+++ apps/fithm-service/manage.py
@@ -1,22 +1,18 @@
 from flask.cli import FlaskGroup, AppGroup
 from libs.database import create_tables, drop_tables, populate_default
-from main import app
-
-cli = FlaskGroup(app)
+from main import create_app
 
 db_cli = AppGroup('db')
+
 
 @db_cli.command('create')
 def create_db():
     create_tables()
     populate_default()
 
+
 @db_cli.command('drop')
 def drop_db():
     drop_tables()
 
-app.cli.add_command(db_cli)
-
-
-if __name__ == '__main__':
-    cli()
+create_app().cli.add_command(db_cli)
